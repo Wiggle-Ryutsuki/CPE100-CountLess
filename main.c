6104ab9e07d9f9372566f5e6286f49e335cfdac1
Maimoona <maimoonaaz15@gmail.com>
2024-12-04 00:22:36 +0700

    Initialize main script

---
+++ main.c
@@ -0,0 +1,8 @@
+// This script is for running and testing the entire program
+
+#include <stdio.h>
+// include other necessary libraries including from other scripts
+
+int main(void){
+    printf("Hello, World!");
+}
