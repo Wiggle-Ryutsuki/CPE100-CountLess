cb848f1249ef00d7a10628b1919f521e733992f1
Maimoona <maimoonaaz15@gmail.com>
2024-12-04 00:23:24 +0700

    Initialize data-handling scripts

---
+++ interface.c
@@ -0,0 +1,32 @@
+// This script is for maintaining the database
+
+#include <stdio.h>
+// include other necessary libraries including from other scripts
+
+
+void displayMenu();
+void getMenuSelection();
+void getUserInput();
+void displayMessage();
+
+
+
+// Function that displays the menu | Change return type to appropriate type
+void displayMenu(){
+    // Code here
+}
+
+// | Change return type to appropriate type
+void getMenuSelection(){
+    // Code here
+}
+
+// Functions that handles user input
+void getUserInput(){
+    // Code here
+}
+
+// Function that outputs feedback
+void displayMessage(){
+    // Code here
+}
