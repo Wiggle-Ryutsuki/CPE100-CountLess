0f86c4c3b991ca2dc1da2f1d598fd6680e73cb8a
Maimoona <maimoonaaz15@gmail.com>
2024-12-04 00:22:53 +0700

    Initialize owner and customer scripts

---
+++ customer_features.c
@@ -0,0 +1,49 @@
+// This script is for programming all customer features
+
+#include <stdio.h>
+// include other necessary libraries including from other scripts
+
+
+void searchProduct();
+void browseProducts();
+void addToCart();
+void viewCart();
+void checkoutCart();
+void updateInventoryAfterPurchase();
+void applyCouponAtCheckout();
+
+
+// Function that searches for a product | Change return type to appropriate type
+void searchProduct(){
+    // Code here
+}
+
+// Function that calls for all products | Change return type to appropriate type
+void browseProducts(){
+    // Code here
+}
+
+// Function that adds product to shopping cart | Change return type to appropriate type
+void addToCart(){
+    // Code here
+}
+
+// Function that calls for all items in cart | Change return type to appropriate type
+void viewCart(){
+    // Code here
+}
+
+// Function that proceeds to checkout point (calculates total and prompts to use coupon) | Change return type to appropriate type
+void checkoutCart(){
+    // Code here
+}
+
+// Function that updates inventory | Change return type to appropriate type
+void updateInventoryAfterPurchase(){
+    // Code here
+}
+
+// Function that applies coupon | Change return type to appropriate type
+void applyCouponAtCheckout(){
+    // Code here
+}
---
+++ owner_features.c
@@ -0,0 +1,62 @@
+// This script is for programming all owner features
+
+#include <stdio.h>
+// include other necessary libraries including from other scripts
+
+
+void addProduct();
+void viewProduct();
+void editProduct();
+void deleteProduct();
+void restockProduct();
+void logAction();
+void createCoupon();
+void validateCoupon();
+void applyCoupon();
+
+
+// Function that creates new product | Change return type to appropriate type
+void addProduct(){
+    // Code here
+}
+
+// Function that views a products information | Change return type to appropriate type
+void viewProduct(){
+    // Code here
+}
+
+// Function that edits a products information | Change return type to appropriate type
+void editProduct(){
+    // Code here
+}
+
+// Function that deletes a product | Change return type to appropriate type
+void deleteProduct(){
+    // Code here
+}
+
+// Function that restocks a product | Change return type to appropriate type
+void restockProduct(){
+    // Code here
+}
+
+// Function that logs action | Change return type to appropriate type
+void logAction(){
+    // Code here
+}
+
+// Function that creates coupon | Change return type to appropriate type
+void createCoupon(){
+    // Code here
+}
+
+// Function that validates coupon when customer uses a coupon | Change return type to appropriate type
+void validateCoupon(){
+    // Code here
+}
+
+// Function that applies coupon when validated | Change return type to appropriate type
+void applyCoupon(){
+    // Code here
+}
+
